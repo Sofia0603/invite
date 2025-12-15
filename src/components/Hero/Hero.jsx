9437505e49350b68ad7c0139d2153e2f0c70c6ef
Sofia Shestakova <shestakova.sofia2016@mail.ru>
2025-12-15 18:01:36 +0400

    new

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,7 +2,12 @@ function Hero(){
   return (
    <section>
      <div className=""></div>
-     <div className="font-ballet text-[115px] leading-none italic text-primary">15/08/26</div>
+     <div className="font-ballet text-[95px] leading-none italic text-primary mb-6">15/08/26</div>
+     <h1 className="font-cafeparis text-[54px] text-primary flex flex-col justify-between leading-none gap-1">
+       <span className="self-start">ЕЛИЗАВЕТА</span>
+       <span className="text-[37px]">&</span>
+       <span className="self-end">МИХАИЛ</span>
+     </h1>
    </section>
   )
 }
